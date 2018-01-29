a639d21b17d1d0ab9d3b17383a13bc83c2a2d352
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-01-29 17:11:25 +0100

    [W3-001] Basic blogger interface added

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,11 @@
-navigateTo("home-content");
+function loginUser() {
+	document.getElementById('login-screen').style.display = "none";
+	document.getElementById('user-interface').style.display = "block";
+	navigateTo('home-content');
+}
+
+function loginBlogger() {
+	document.getElementById('login-screen').style.display = "none";
+	document.getElementById('blogger-interface').style.display = "block";
+	navigateTo('create-article');
+}
